aa2bdf4ec35d9bcc6acc1a8e2f983d6dc88d1c99
Xpomin <s-hromin@mail.ru>
2022-10-04 17:08:39 +0700

    Release

--- src/lab3.c
+++ src/lab3.c
@@ -1,8 +1,8 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 GLfloat SPIN = 0.0;
 GLfloat SPEED = 2.0;
@@ -10,26 +10,36 @@ GLfloat SPEED = 2.0;
 #define WIN_HEIGHT 800
 #define WIN_WIDTH 800
 
-
-GLdouble SMALL_QUAD_X = WIN_WIDTH / 2 *0.8;
-GLdouble SMALL_QUAD_Y = WIN_HEIGHT / 2 * 0.8;
+GLdouble SMALL_QUAD_X = -WIN_WIDTH / 2 * 0.8;
+GLdouble SMALL_QUAD_Y = -WIN_HEIGHT / 2 * 0.8;
 GLdouble SMALL_QUAD_SIZE = 60;
 
 GLdouble MOUSE_LAST_X = 0;
 GLdouble MOUSE_LAST_Y = 0;
 
+char IS_LEFT_BUTTON_PRESSED = 0;
+
 void init();
 void display();
-void drawSquad(GLdouble x1, GLdouble y1, GLdouble x2, GLdouble y2, GLdouble x3,
-               GLdouble y3, GLdouble x4, GLdouble y4);
+
+void drawSquadByCenter(GLdouble centerX, GLdouble centerY, GLdouble size);
+
 void spinDisplay();
 void reshape(int w, int h);
+
 void Keyboard(unsigned char key, int x, int y);
 void Mouse(int button, int state, int x, int y);
 void onMouseMotion(int x, int y);
-void drawSquadByCenter(GLdouble centerX, GLdouble centerY, GLdouble size);
 
-int main(int argc, char** argv) {
+void saveLastMouseCoord(int x, int y);
+void displaceCoords(int *x, int *y);
+
+int IsCoordInSmallSqaud(int x, int y);
+int IsLeftButtonPressedAndHolding(unsigned char button, int state);
+int IsLeftButtonPressedAndReleased(unsigned char button, int state);
+int IsRightButtonPressedAndReleased(unsigned char button, int state);
+
+int main(int argc, char **argv) {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
   glutInitWindowSize(WIN_HEIGHT, WIN_HEIGHT);
@@ -60,17 +70,12 @@ void display() {
   glRotatef(SPIN, 0.0, 0.0, 1.0);
   glColor3f(1.0, 1.0, 1.0);
 
-
-  drawSquad(
-          -WIN_WIDTH / 8, 0,
-          0, WIN_HEIGHT / 8,
-          WIN_WIDTH / 8, 0,
-          0, -WIN_HEIGHT / 8);
+  drawSquadByCenter(0, 0, WIN_HEIGHT * 0.4);
 
   glPopMatrix();
   glColor3f(0.6, 0.2, 0.35);
   drawSquadByCenter(SMALL_QUAD_X, SMALL_QUAD_Y, SMALL_QUAD_SIZE);
-          
+
   glPopMatrix();
   glutSwapBuffers();
 }
@@ -84,23 +89,9 @@ void drawSquadByCenter(GLdouble centerX, GLdouble centerY, GLdouble size) {
   glEnd();
 }
 
-void drawSquad(GLdouble x1, GLdouble y1, 
-              GLdouble x2, GLdouble y2,
-               GLdouble x3, GLdouble y3, 
-               GLdouble x4, GLdouble y4) {
-  glBegin(GL_POLYGON);
-  {
-    glVertex2f(x1, y1);
-    glVertex2f(x2, y2);
-    glVertex2f(x3, y3);
-    glVertex2f(x4, y4);
-  }
-  glEnd();
-}
-
 void spinDisplay() {
   SPIN = SPIN + SPEED;
-  if (SPIN > 360.0) SPIN = SPIN - 360.0;
+  if (SPIN > 360.0) SPIN -= -360.0;
   glutPostRedisplay();
 }
 
@@ -120,41 +111,54 @@ void Keyboard(unsigned char key, int x, int y) {
   if (key == 'x') SPEED = 2.0;
 }
 
-char IS_LEFT_BUTTON_PRESSED = 0;
-
 void Mouse(int button, int state, int x, int y) {
-  MOUSE_LAST_X = x;
-  MOUSE_LAST_Y = y;
+  saveLastMouseCoord(x, y);
+  displaceCoords(&x, &y);
 
-  x -= WIN_WIDTH / 2;
-  y = -y + WIN_HEIGHT / 2;
-
-  if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN  &&
-    x > SMALL_QUAD_X - SMALL_QUAD_SIZE / 2 &&
-    x <  SMALL_QUAD_X + SMALL_QUAD_SIZE / 2 &&
-    y > SMALL_QUAD_Y - SMALL_QUAD_SIZE / 2 &&
-    y < SMALL_QUAD_Y + SMALL_QUAD_SIZE / 2
-) {
+  if (IsLeftButtonPressedAndHolding(button, state) && IsCoordInSmallSqaud(x, y))
     IS_LEFT_BUTTON_PRESSED = 1;
-  } else {
-    printf("x == %d y == %d %f %f \n", x, y, SMALL_QUAD_X, SMALL_QUAD_Y);
+  else
     IS_LEFT_BUTTON_PRESSED = 0;
-  }
 
-  if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
-    SPEED = 2.0;
-  }
+  if (IsLeftButtonPressedAndReleased(button, state)) SPEED = 2.0;
+  if (IsRightButtonPressedAndReleased(button, state)) SPEED = 0.0;
+}
 
-  if (button == GLUT_RIGHT_BUTTON && state == GLUT_UP) SPEED = 0.0;
+int IsCoordInSmallSqaud(int x, int y) {
+  return (x > SMALL_QUAD_X - SMALL_QUAD_SIZE / 2 &&
+          x < SMALL_QUAD_X + SMALL_QUAD_SIZE / 2 &&
+          y > SMALL_QUAD_Y - SMALL_QUAD_SIZE / 2 &&
+          y < SMALL_QUAD_Y + SMALL_QUAD_SIZE / 2);
 }
 
 void onMouseMotion(int x, int y) {
-  if (
-    IS_LEFT_BUTTON_PRESSED) {
+  if (IS_LEFT_BUTTON_PRESSED) {
     SMALL_QUAD_X += x - MOUSE_LAST_X;
     SMALL_QUAD_Y += MOUSE_LAST_Y - y;
   }
 
   MOUSE_LAST_X = x;
   MOUSE_LAST_Y = y;
+}
+
+void saveLastMouseCoord(int x, int y) {
+  MOUSE_LAST_X = x;
+  MOUSE_LAST_Y = y;
+}
+
+void displaceCoords(int *x, int *y) {
+  *x -= WIN_WIDTH / 2;
+  *y = -(*y) + WIN_HEIGHT / 2;
+}
+
+int IsLeftButtonPressedAndHolding(unsigned char button, int state) {
+  return (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN);
+}
+
+int IsLeftButtonPressedAndReleased(unsigned char button, int state) {
+  return (button == GLUT_LEFT_BUTTON && state == GLUT_UP);
+}
+
+int IsRightButtonPressedAndReleased(unsigned char button, int state) {
+  return (button == GLUT_RIGHT_BUTTON && state == GLUT_UP);
 }
